5b39fd3fc741cc6d2425e9a314aca1dd4fdfcc5b
Dias <diasraphael88@gmail.com>
2021-03-14 15:03:51 +0100

    Added classic editor

---
+++ src/views/Editor.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ckeditor
+      :editor="editor"
+      v-model="editorData"
+      :config="editorConfig"
+    ></ckeditor>
+  </div>
+</template>
+<script>
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import CKEditor from "@ckeditor/ckeditor5-vue";
+export default {
+  components: {
+    // Use the <ckeditor> component in this view.
+    ckeditor: CKEditor.component
+  },
+  data() {
+    return {
+      editor: ClassicEditor,
+      editorData: "This is a sample text in ckeditor vue",
+      editorConfig: {}
+      // ...
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+a {
+  color: #42b983;
+}
+</style>
